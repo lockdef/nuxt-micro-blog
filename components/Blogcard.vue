62136e57e93fbad1ac3bbd526622e21846665850
lockdef <locktki@icloud.com>
2020-06-02 14:33:23 +0900

    命名規則の変更

--- components/Blogcard.vue
+++ components/Blogcard.vue
@@ -11,6 +11,7 @@
 
 <script>
 export default {
+  name: "BlogPost"
   props: ['title', 'text']
 }
 </script>
---
+++ components/Blogpost.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    :lazy-validation="lazy"
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    />
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    />
+
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Item"
+      required
+    />
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    />
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Validate
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+
+    <v-btn
+      color="warning"
+      @click="resetValidation"
+    >
+      Reset Validation
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'Blogpost'
+}
+</script>
