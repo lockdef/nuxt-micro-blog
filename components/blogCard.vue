f4c1491c6ac21a3f58430cbc94f5cabff1faca03
lockdef <locktki@icloud.com>
2020-06-02 14:12:51 +0900

    コンテンツを表現するカードコンポーネントを追加

---
+++ components/blogCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card class="mx-auto">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1" v-text="title" />
+        <v-card-text v-text="text" />
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['title', 'text']
+}
+</script>
